beb24ea2123dea9a647e8339c18bb09c6fe5fb95
VictorGuiVasco <victorguilhermecavalcante@gmail.com>
2021-01-04 19:27:30 -0300

    aula 01

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,33 @@
+import react from 'react'
+import { Link } from 'react-router-dom'
+
+import '../styles/pages/landing.css'
+import { FiArrowRight } from 'react-icons/fi'
+
+import logoImg from '../assets/logo.svg'
+
+function Landing() {
+  return(
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Manaus</strong>
+          <span>Amazonas</span>
+        </div>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={24} color="rgb(0,0,0,0.6)" />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
